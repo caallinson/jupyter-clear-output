840e1b85e0ad1b9f37404740b37815a8d1046c0b
Christian <caallins@mit.edu>
2020-10-02 22:20:28 -0400

    working

--- clean-notebook-output.py
+++ clean-notebook-output.py
@@ -1,18 +1,63 @@
 import json
-import sys
-import pytest
+import sys  
+
+def process_json(json_parsed):
+    
+    if isinstance(json_parsed, dict):
+        for obj in json_parsed['cells']:
+            if 'outputs' in obj.keys():
+                obj['outputs'] = []
+    else:
+        return None
+
+    return json_parsed
 
 def main():
     if len(sys.argv) == 1:
-        print('\tError: Provide Jupyter notebook to clean in command line')
-        return 0
-    fName = sys.argv[1]
+        print('\tClean Output of Jupyter Notebook Files')
+        print('\tError: Provide Jupyter notebook file path')
+        print('\t\t-f : Force read of non-ipynb file')
+        return 1
+
+    fName = None
+    for arg in sys.argv[1:]:
+        if arg[0] != '-':
+            fName = arg
+    if fName is None:
+        print('\tError: File name not provided')
+        return 1
+
+    if fName.lower()[-6:] != ".ipynb":
+        if '-f' not in sys.argv:
+            print('\tError: File not .ipynb extension')
+            print('\t\tUse -f to force')
+            return 1
+
     try:
         fRead = open(fName)
-    except OSError as err:
+    except Exception as err:
         print("\tError in reading file")
         print(err)
+        return 1
 
+    try:
+        f_contents = json.load(fRead)
+    except Exception as err:
+        print("\tError Parsing JSON")
+        print(err)
+        return 1
+
+    fRead.close()
+    f_contents = process_json(f_contents)
+
+    if f_contents is not None:
+        fWrite = open(fName, 'w')
+        json.dump(f_contents, fWrite, indent=1)
+    else:
+        print("\tError: Couldn't process JSON")
+        return 1
+
+    return 0
 
 if __name__ == "__main__":
     main()
--- tests/test_lean-notebook-output.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_one():
-    assert 1
